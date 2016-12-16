e620aa53304704d1396ba3833949c11b05c201f7
Pritin Patel <pritinp@hotmail.com>
2016-12-16 17:19:19 +0000

    update

---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string myValue = superSecretFormula("world");
+        //    Console.WriteLine(myValue);
+        //    Console.ReadLine();
+        //}
+
+        //private static string superSecretFormula()
+        //{
+        //    // some cool stuff here
+        //    return "Hello World!";
+        //}
+
+        //private static string superSecretFormula(string name)
+        //{
+        //    return String.Format("Hello, {0}!", name);
+        //}
+
+
+// this version of the code passes an integer, and c#/VS automaticaly knows to pass this to the 'integer' method - an Overloaded method!
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula(123);
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+        // Overloaded Method 1
+        private static string superSecretFormula(int aNumber)
+        {
+            // some cool stuff here
+            return String.Format("Hello World!, {0}", aNumber);
+        }
+
+        // Overloaded Method 2
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello, {0}!", name);
+        }
+
+        private static void superSecretFormula2()
+        {
+
+        }
+    }
+}
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myString = "Go to your c:\\ drive";
+            //string myString = "My \"so called\" life";
+            //string myString = "What if I need \n a new line";
+
+            //string myString = string.Format("{0}!", "Bonzai");
+            //string myString = string.Format("Make: {0} (Model: {1})", "BMW", "760li");
+
+            //string myString = string.Format("{0:C}", 123.45);
+            //string myString = string.Format("{0:N}", 123456789);
+            //string myString = string.Format("{0:P}", .70);
+            string myString = string.Format("Phone number: {0:(###) ###-####}", 1234567890);
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
