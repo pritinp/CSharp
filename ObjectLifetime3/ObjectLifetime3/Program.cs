fc06f074c4e4c17a609e49403779d46c8158936d
Pritin Patel <pritinp@hotmail.com>
2017-01-10 16:54:43 +0000

    update

---
+++ ObjectLifetime3/ObjectLifetime3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            // Create a another refernce to the same object above 
+            Car myOtherCar = myCar;
+
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "White"); 
+
+            myOtherCar = null;
+            myCar = null;
+
+            
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        // Theis class contains a Constructor to set the name of the car whenever the class is created...
+        public Car()
+        {
+            this.Make = "Nissan";
+        }
+
+        // Add another method to the class - an overlaoded method
+        public Car(String make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        //public Car(string someOtherInputParameter, string model, int year, string color)
+        //{
+        //    Make = someOtherInputParameter;
+        //    Model = model;
+        //    Year = year;
+        //    Color = color;
+        //}
+
+        public static void MyMethod()
+        {
+
+        }
+}
---
+++ UnderstandingInheritance/UnderstandingInheritance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "BMW";
+            myCar.Model = "745li";
+            myCar.Color = "Black";
+            myCar.Year = 2005;
+
+            // calls method 'printCarDetails'
+            printCarDetails(myCar);
+
+            Truck myTruck -new Truck();
+
+            myTruck.Make = "Ford";
+            myTruck.Model = "F950";
+
+
+            Console.ReadLine();
+        }
+
+        // this method doesn't return anything as it's set to 'void'
+        // why are the parameters 'Car car' passed in here?
+        private static void printCarDetails(Car car)
+        {
+            Console.WriteLine("Here are the Car's details: {0}",
+                car.FormatMe());
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public string FormatMe()
+        {
+            return String.Format("{0} - {1} - {2} - {3}",
+                this.Make,
+                this.Model,
+                this.Color,
+                this.Year);
+        }
+    }
+
+    class Truck : Car
+    {
+        public int TowingCapacity { get; set; }
+    }
+}
