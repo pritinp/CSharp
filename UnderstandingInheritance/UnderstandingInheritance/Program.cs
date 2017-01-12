d8b56fc93255b38cb9132c5c485a4eb47c71d79f
Pritin Patel <pritinp@hotmail.com>
2017-01-12 15:49:36 +0000

    update

--- UnderstandingInheritance/UnderstandingInheritance/Program.cs
+++ UnderstandingInheritance/UnderstandingInheritance/Program.cs
@@ -19,22 +19,30 @@ namespace UnderstandingInheritance
             // calls method 'printCarDetails'
             printCarDetails(myCar);
 
-            Truck myTruck -new Truck();
-
-            myTruck.Make = "Ford";
-            myTruck.Model = "F950";
-
-
             Console.ReadLine();
         }
 
-        // this method doesn't return anything as it's set to 'void'
-        // why are the parameters 'Car car' passed in here?
-        private static void printCarDetails(Car car)
+        /*
+         -this method doesn't return anything as it's set to 'void'
+         -why are the parameters 'Car car' passed in here? The small case 'car' is the input parameter, and the large case 'Car' is the datatype.Google Passing datatypes into methods.
+         -Therefore, I think it's essentialy doing the same as the following...
+              private static void printCarDetails(string inputParameter) // where 'string' is the 'data type'
+         -reference from video: Video 15 @ 18m 43sec - input parameter names
+         */
+        private static void printCarDetails(Car _car)
         {
             Console.WriteLine("Here are the Car's details: {0}",
-                car.FormatMe());
+                _car.FormatMe());
         }
+
+        // the variable in the helper method above could also be prefixed with a underscore to make it clearer it is an Input Parameter
+        /*
+        private static void printCarDetails(Car _car)
+        {
+            Console.WriteLine("Here are the Car's details: {0}",
+                _car.FormatMe());
+        }
+        */
     }
 
     class Car
@@ -44,7 +52,7 @@ namespace UnderstandingInheritance
         public int Year { get; set; }
         public string Color { get; set; }
 
-        public string FormatMe()
+        public virtual string FormatMe()
         {
             return String.Format("{0} - {1} - {2} - {3}",
                 this.Make,
@@ -54,8 +62,17 @@ namespace UnderstandingInheritance
         }
     }
 
-    class Truck : Car
-    {
-        public int TowingCapacity { get; set; }
-    }
+    //class Truck : Car
+    //{
+    //    public int TowingCapacity { get; set; }
+
+    //    public string FormatMe()
+    //    {
+    //        return String.Format("{0} - {1} - {2} Towing Units",
+    //            this.Make,
+    //            this.Model,
+    //            this.TowingCapacity);
+    //    }
+
+    //}
 }
