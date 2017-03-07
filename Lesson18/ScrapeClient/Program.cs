c19fb4fad815fbc465e5459c46b4e3a029745bf3
Pritin Patel <pritinp@hotmail.com>
2017-03-07 15:29:45 +0000

    update

---
+++ Lesson18/ScrapeClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using ScrapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string websiteUrl = "https://msdn.microsoft.com";
+            string filepath = @"D:\ITStudying\BobTaborCourse\Lesson17\WriteText2.txt";
+
+            Scrape myScrape = new Scrape();
+            string urlValue = myScrape.ScrapeWebPage(websiteUrl, filepath);
+            Console.WriteLine(urlValue);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyCodeLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string websiteUrl = "https://msdn.microsoft.com";
+            string filepath = @"D:\ITStudying\BobTaborCourse\Lesson17\WriteText2.txt";
+
+            Scrape myScrape = new Scrape();
+            string urlValue = myScrape.ScrapeWebPage(websiteUrl, filepath);
+            Console.WriteLine(urlValue);
+            Console.ReadLine();
+
+
+            //Scrape myScrape = new Scrape();
+            //string value = myScrape.ScrapeWebPage("https://msdn.microsoft.com");
+            //Console.WriteLine(value);
+            //Console.ReadLine();
+        }
+    }
+}
