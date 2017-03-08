545631ebfad036ae854411a160a29bceb1f1bf18
Pritin Patel <pritinp@hotmail.com>
2017-03-08 17:19:58 +0000

    update

---
+++ TimerConsoleApp/TimerConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 2000);
+        }
+
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine("In TimerCallback: " + DateTime.Now);
+            GC.Collect();
+        }
+    }
+}
