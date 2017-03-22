89e2dac67176f54b03004a31fdf6e50ea445e2e3
Pritin Patel <pritinp@hotmail.com>
2017-03-22 16:42:54 +0000

    update

---
+++ BasicExcercises/BasicExcercises/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExcercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Excercises taken from...
+            // http://www.w3resource.com/csharp-exercises/basic/index.php
+
+            //// 1. Write a C# Sharp program to print Hello and your name in a separate line.
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Pritin Patel");
+            //Console.ReadLine();
+
+            ////2.Write a C# Sharp program to print the sum of two numbers.
+            //int x = 20;
+            //int y = 40;
+            //int z;
+
+            //z = x + y;
+            //Console.WriteLine("The sum of 2 numbers is: {0}", z);
+            //Console.ReadLine();
+
+            ////3.Write a C# Sharp program to print the result of dividing two numbers.
+            //int x = 8;
+            //int y = 2;
+            //int z;
+
+            //z = x / y;
+            //Console.WriteLine("The result of dividing 2 numbers is: {0}", z);
+            //Console.ReadLine();
+
+            // using specified solution...
+            //Console.WriteLine(8 / 2);
+            //Console.ReadLine();
+
+
+            //4.Write a C# Sharp program to print the result of the specified operations...
+            //-1 + 4 * 6
+            //(35 + 5) % 7
+            //14 + -4 * 6 / 11
+            //2 + 15 / 6 * 1 - 7 % 2
+
+            int x = 2 + 15 / 6 * 1 - 7 % 2;
+
+            Console.WriteLine("{0}", x);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
--- WorkingwithLINQ/Before/UnderstandingLINQ/UnderstandingLINQ/Program.cs
+++ WorkingwithLINQ/Before/UnderstandingLINQ/UnderstandingLINQ/Program.cs
@@ -38,13 +38,30 @@ namespace UnderstandingLINQ
 
             //Console.WriteLine(myCars.TrueForAll(p => p.Year > 2007));
 
-            myCars.ForEach(p => p.StickerPrice -= 3000);
-            myCars.ForEach(p => Console.WriteLine("{0} {1:C}", p.VIN, p.StickerPrice));
+            //myCars.ForEach(p => p.StickerPrice -= 3000);
+            //myCars.ForEach(p => Console.WriteLine("{0} {1:C}", p.VIN, p.StickerPrice));
+
+            //Console.WriteLine(myCars.Exists(p => p.Model == "745li"));
+
+            //Console.WriteLine("{0:c}", myCars.Sum(p => p.StickerPrice));
 
             //foreach (var car in orderedCars)
             //{
             //    Console.WriteLine("{0} {1}", car.Year, car.Model, car.VIN);
             //}
+
+            Console.WriteLine(myCars.GetType());
+            var orderedCars = myCars.OrderByDescending(p => p.Year);
+            Console.WriteLine(orderedCars.GetType());
+
+            var bmws = myCars.Where(p => p.Make == "BMW" && p.Year == 2010);
+            Console.WriteLine(bmws.GetType());
+
+            var newCars = from car in myCars
+                       where car.Make == "BMW"
+                       && car.Year == 2010
+                       select new { car.Make, car.Model };
+            Console.WriteLine(newCars.GetType());
 
             Console.ReadLine();
 
