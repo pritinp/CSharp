87a2a63b8eb7d077ebd54d6da7e27795507e2238
Pritin Patel <pritinp@hotmail.com>
2017-04-05 15:51:30 +0100

    update

---
+++ BooleanTEsting/BooleanTEsting/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanTEsting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = true;
+
+            if (a && b)
+            {
+                Console.WriteLine("Line 1 - Condition is true");
+            }
+            else
+            {
+                Console.WriteLine("Line 1 - Condition is false");
+            }
+
+            if (b && a)
+            {
+                Console.WriteLine("Line 2 - Condition is true");
+            }
+            else
+            {
+                Console.WriteLine("Line 2 - Condition is false");
+            }
+
+            //change the vlaue of b to false
+            bool c = true;
+            bool d = false;
+
+            if (c && d)
+            {
+                Console.WriteLine("Line 3 - Condition is true");
+            }
+            else
+            {
+                Console.WriteLine("Line 3 - Condition is false");
+            }
+
+            if (d && c)
+            {
+                Console.WriteLine("Line 4 - Condition is true");
+            }
+            else
+            {
+                Console.WriteLine("Line 4 - Condition is false. The && operator only evaluates the first condition because it's false. The & operator would evluate both conditons");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ForLoopExcercises/ForLoopExcercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopExcercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 1. Write a program in C# Sharp to display the first 10 natural numbers.
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("{0}\n", i);
+            //}
+            //Console.Write("\n\n");
+            //Console.ReadLine();
+
+            //// 2. Write a C# Sharp program to find the sum of first 10 natural numbers.
+            // int sum = 1;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("On pass number {0} Value of y: {1}", i, sum);
+            //    sum += i;
+            //}
+            //Console.ReadLine();
+
+            //// 3.Write a program in C# Sharp to display n terms of natural number and their sum.
+            //int term;
+            //int sum = 0;
+            //Console.WriteLine("Enter the number of terms: ");
+            //term = int.Parse(Console.ReadLine());
+            //Console.Write("The first {0} natural numbers is: ", term);
+            //for (int i = 1; i <= term; i++)
+            //{
+            //    Console.Write("{0} ", i);
+            //    sum += i;
+            //}
+            //Console.Write("\nThe sum of the first {0} natural numbers is: {1}", term, sum);
+            //Console.ReadLine();
+
+            // 4. Write a program in C# Sharp to read 10 numbers from keyboard and find their sum and average.
+
+        }
+    }
+}
